47d66b6761925d5f40ec2c1f23f27b9b6f0effbf
Bayu Indra Kusuma <bayuindrakusuma05@gmail.com>
2021-02-08 05:52:03 +0700

    Add form and button

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
-  String _name = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,10 +28,12 @@ class MainScreen extends StatelessWidget {
                     color: Colors.black12,
                   ),
                   child: TextField(
-                    onChanged: (String value) => _name = value,
                     decoration: InputDecoration(
-                        border: new UnderlineInputBorder(
-                            borderSide: new BorderSide(color: Colors.purple)),
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.purple)),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.purple, width: 2)),
                         labelText: 'Card Name',
                         labelStyle: TextStyle(color: Colors.grey),
                         hintText: 'Musa Adebola',
@@ -48,7 +49,7 @@ class MainScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
                 Icon(
-                  Icons.credit_card,
+                  Icons.card_membership,
                   size: 35,
                   color: Colors.grey,
                 ),
@@ -59,15 +60,90 @@ class MainScreen extends StatelessWidget {
                   ),
                   child: TextField(
                     decoration: InputDecoration(
-                        border: new UnderlineInputBorder(
-                            borderSide: new BorderSide(color: Colors.purple)),
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.purple)),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.purple, width: 2)),
                         labelText: 'Number',
                         labelStyle: TextStyle(color: Colors.grey),
-                        hintText: '5060 6666 6666 6666 666',hintStyle: TextStyle(color: Colors.black)),
+                        hintText: '5060 6666 6666 6666 666',
+                        hintStyle: TextStyle(color: Colors.black)),
                   ),
                 )
               ],
             ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Icon(
+                  Icons.credit_card,
+                  size: 35,
+                  color: Colors.grey,
+                ),
+                Container(
+                  width: 330,
+                  decoration: BoxDecoration(color: Colors.black12),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.purple)),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.purple, width: 2)),
+                        labelText: 'CVV',
+                        labelStyle: TextStyle(color: Colors.grey),
+                        hintText: '687',
+                        hintStyle: TextStyle(color: Colors.black)),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Icon(
+                  Icons.date_range,
+                  size: 35,
+                  color: Colors.grey,
+                ),
+                Container(
+                  width: 330,
+                  decoration: BoxDecoration(color: Colors.black12),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.purple)),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.purple, width: 2)),
+                        labelText: 'Expire Date',
+                        labelStyle: TextStyle(color: Colors.grey),
+                        hintText: '10/20',
+                        hintStyle: TextStyle(color: Colors.black)),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 50,),
+          MaterialButton(
+            height: 50,
+            minWidth: 170,
+            shape: RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(10)),
+            onPressed: () {},
+            child: Text(
+              'Pay',
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            ),
+            color: Colors.blueAccent,
           )
         ],
       ),
